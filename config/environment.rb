c338debc0b6ec416a0f24a80418b49a5df271332
madhuri2904 <banoriyamadhuri@gmail.com>
2025-07-06 00:51:24 +0530

    Debug ENV key

--- config/environment.rb
+++ config/environment.rb
@@ -2,18 +2,21 @@
 require_relative "application"
 
 if Rails.env.production?
-  require "openssl"
-  cipher = OpenSSL::Cipher.new('aes-128-cbc')
-  cipher.encrypt
-
-  key = ENV['ENCRYPTION_SECRET']
-
-  unless key && key.bytesize == 16
-    raise "ENCRYPTION_SECRET must be exactly 16 bytes. Set it in Render Environment Variables."
+    require "openssl"
+    cipher = OpenSSL::Cipher.new('aes-128-cbc')
+    cipher.encrypt
+  
+    key = ENV['ENCRYPTION_SECRET']
+  
+    puts "🔐 ENV['ENCRYPTION_SECRET'] = #{key.inspect}"
+    puts "🔐 Length = #{key&.bytesize}"
+  
+    unless key && key.bytesize == 16
+      raise "ENCRYPTION_SECRET must be exactly 16 bytes. Got: #{key&.bytesize || 0}"
+    end
+  
+    cipher.key = key
   end
-
-  cipher.key = key
-end
 
 # Initialize the Rails application.
 Rails.application.initialize!
